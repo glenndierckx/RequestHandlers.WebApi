698c323de6b8e6187e56be47c74e23360d91cba9
Glenn Dierckx <glenn@windstyle.be>
2017-08-09 16:43:06 +0200

    Add test project

---
+++ src/RequestHandlers.WebApi.Tests/UnitTest1.cs
@@ -0,0 +1,13 @@
+using System;
+using Xunit;
+
+namespace RequestHandlers.WebApi.Tests
+{
+    public class UnitTest1
+    {
+        [Fact]
+        public void Test1()
+        {
+        }
+    }
+}
